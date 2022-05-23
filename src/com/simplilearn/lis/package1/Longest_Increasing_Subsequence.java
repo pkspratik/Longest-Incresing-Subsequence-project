dc92e15905d4646213b884d06d53b199cd8da0d8
Pratik <pratikk512@gmail.com>
2022-05-23 17:09:37 +0530

    Longest incresing Subsequence find

---
+++ src/com/simplilearn/lis/package1/Longest_Increasing_Subsequence.java
@@ -0,0 +1,52 @@
+package com.simplilearn.lis.package1;
+import java.util.Scanner;
+public class Longest_Increasing_Subsequence {
+
+	static int arraysize ;  // static variable
+	static int array[];     //static variable
+	 int i, j, max = 0;    // instance variable
+	 int lis[] ;     // instance variable
+	
+	 public static void main(String[] args) {
+
+		 Scanner sc = new Scanner(System.in);  
+		 System.out.println("Enter array length ");
+		 arraysize = sc.nextInt();
+		 array = new int[arraysize]; 
+
+		 Longest_Increasing_Subsequence obj = new Longest_Increasing_Subsequence();  // creating object ref 
+
+		 System.out.println("Enter add element in array");
+		 for(int i=0; i<array.length; i++)
+		 {
+			 System.out.print("array ["+i+"] = ");
+			 array[i] = sc.nextInt(); 
+		 }
+		 System.out.println();
+		 long l = array.length;
+		 int n= (int)l;    // use here explicit type casting
+		 System.out.println("Length of Longest Increasing Subsequence is " + obj.lis(array, n) + "\n");
+	 }
+	
+	private final int lis(int arr[], int n)    // this is private with final method we can access within a class
+	 {
+		 lis = new int[n];   // this is implicitly present but i write explicitly 
+
+		 /// Initialize LIS values for all indexes 
+		 for (i = 0; i < n; i++)
+			 lis[i] = 1;
+
+		 // Compute optimized LIS values in bottom up manner 
+		 for (i = 1; i < n; i++)
+			 for (j = 0; j < i; j++)
+				 if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+					 lis[i] = lis[j] + 1;
+
+		 // find maximum of all LIS values 
+		 for (i = 0; i < n; i++)
+			 if (max < lis[i])  
+				 max = lis[i];  
+
+		 return this.max;   // i used this here explicitly 
+	 }
+}
